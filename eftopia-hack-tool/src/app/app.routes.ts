9caeec302a765fca74ff0093abe260451bbe3547
PanasHadji <107615372+PanasHadji@users.noreply.github.com>
2024-11-16 17:34:55 +0200

    search page

--- eftopia-hack-tool/src/app/app.routes.ts
+++ eftopia-hack-tool/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'welcome',
+        pathMatch: 'full'
+      },
+      {
+        path: 'welcome',
+        loadComponent: () => import('./welcome-page/welcome.component').then(m => m.WelcomeComponent)
+      },
+      {
+        path: 'file-search',
+        loadComponent: () => import('./file-search/file-search.component').then(m => m.FileSearchComponent)
+      }
+    ]
+  }
+];
