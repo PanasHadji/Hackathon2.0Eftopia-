3ea69ea5d763f876773c7dcbdf3e8eafc626a09e
Marios Philippou <mariosphilippou145@gmail.com>
2024-11-17 09:37:25 +0100

    i hate this

--- Backend/app/api.py
+++ Backend/app/api.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
 from pydantic import BaseModel
+from docx import Document
+import PyPDF2
+from bs4 import BeautifulSoup
 
 # Initialize the FastAPI app
 app = FastAPI()
@@ -28,13 +31,47 @@ law_categories = [
 class SearchRequest(BaseModel):
     query: str
 
+
+
+#parse dawgx
+def parse_docx(file_path):
+    doc = Document(file_path)
+    text = []
+    for para in doc.paragraphs:
+        text.append(para.text)
+    return '\n'.join(text)
+
+#parse pdf
+def parse_pdf(file_path):
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = []
+        for page in reader.pages:
+            text.append(page.extract_text())
+        return '\n'.join(text)
+
+#parse html
+def parse_html(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
+        text = soup.get_text()
+    return text
+
+
+
+
+
 @app.post("/search")
 async def search(request: SearchRequest):
+
     text = request.query
+
+    #fetched_cases = ??
+
     result = nlp_pipeline(text, law_categories)
     predicted_label = result["labels"][0]
     probabilities = dict(zip(result["labels"], result["scores"]))
-    return {"query": text, "predicted_label": predicted_label, "probabilities": probabilities}
+    return {"query": text, "predicted_label": predicted_label, "probabilities": probabilities} #TODO "cases": fetched_cases
 
 @app.get("/")
 def read_root():
